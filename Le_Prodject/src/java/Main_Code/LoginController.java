d169bfdbb1887cffce6e986eb8b7509f266a9bfd
Devon35K <153049666+KETSU20@users.noreply.github.com>
2024-11-15 18:24:54 +0800

    First Commit
    NOVMBER 15, 2024

---
+++ Le_Prodject/src/java/Main_Code/LoginController.java
@@ -0,0 +1,47 @@
+package Main_Code;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+
+public class LoginController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchLogin(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("login_view.fxml"));
+        stage =(Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchStaff(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("staffLogin_view.fxml"));
+        stage =(Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    @FXML
+    private Button close;
+    public void close(){
+    System.exit(0);
+    }
+
+
+
+}
+
+
